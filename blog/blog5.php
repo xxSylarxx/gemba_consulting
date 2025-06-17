7bbfc7f1746b59371609793a579888c6ae9837db
Longoria220204 <longrichenrique77@gmail.com>
2025-06-17 09:20:45 -0500

    cambios

--- blog/blog5.php
+++ blog/blog5.php
@@ -4,10 +4,15 @@
 <div>
     <div style="text-align: left;">La semana pasada regres&eacute; de mi primera experiencia trabajando en una mina, como parte del proyecto de Optimizaci&oacute;n del cierre del flujo contable. Fui con el equipo de Gemba Consulting Group y nuestro objetivo era claro: entender desde la ra&iacute;z qu&eacute; est&aacute; ocurriendo en el cierre de mes para identificar oportunidades de mejora junto con el equipo.</div>
     <div style="text-align: left;">Para lograrlo, sab&iacute;amos que no bastaba con revisar documentos o tener reuniones remotas. Hab&iacute;a que estar all&iacute;, en la Gemba, donde realmente ocurren las cosas.</div>
+    <div style="text-align: left;">&nbsp;</div>
     <div style="text-align: left;">Es as&iacute; que, despu&eacute;s de un viaje largo y demandante (3 horas en avi&oacute;n y m&aacute;s de 10 horas por tierra) llegamos a la unidad minera, ubicada a casi 4,500 msnm. Las condiciones clim&aacute;ticas eran extremas y trabajar en altura no fue f&aacute;cil: el cansancio llega m&aacute;s r&aacute;pido, hablar se vuelve dif&iacute;cil y, en mi caso, incluso necesit&eacute; ox&iacute;geno adicional. Pero estar en la operaci&oacute;n vali&oacute; totalmente el esfuerzo.</div>
+    <div style="text-align: left;">&nbsp;</div>
     <div style="text-align: left;">Durante los d&iacute;as que estuvimos ah&iacute;, observamos de cerca los procesos asociados al cierre contable y conversamos m&aacute;s a fondo con los miembros de las distintas &aacute;reas involucradas como: Control de Gesti&oacute;n, Topolog&iacute;a, Administraci&oacute;n de Contratos, Servicios Generales, entre otras. M&aacute;s all&aacute; de lo t&eacute;cnico, lo m&aacute;s valioso fue poder escuchar y preguntar. Cada conversaci&oacute;n nos permiti&oacute; entender mejor las fricciones que enfrenta el equipo, los retrabajos que se generan, los nudos de comunicaci&oacute;n y los esfuerzos que muchas veces se hacen invisibles desde una oficina.</div>
+    <div style="text-align: left;">&nbsp;</div>
     <div style="text-align: left;">Estar en la Gemba nos permiti&oacute; ver con claridad que, detr&aacute;s de cada desviaci&oacute;n en fechas, hay causas concretas. Tambi&eacute;n nos permiti&oacute; ver la disposici&oacute;n de los equipos para mejorar. Hay mucha voluntad, pero tambi&eacute;n necesidad de herramientas que faciliten la mejora continua.</div>
+    <div style="text-align: left;">&nbsp;</div>
     <div style="text-align: left;">De este viaje me llevo aprendizajes clave sobre c&oacute;mo simplificar, estandarizar y alinear mejor el trabajo entre &aacute;reas. Y tambi&eacute;n se reforz&oacute; una convicci&oacute;n: la mejora comienza observando, escuchando y comprendiendo, no desde el escritorio, sino desde el lugar donde realmente se genera el valor.</div>
+    <div style="text-align: left;">&nbsp;</div>
     <div style="text-align: left;">Ahora que estamos de vuelta, seguimos trabajando con la informaci&oacute;n recogida, para acompa&ntilde;ar al equipo en la construcci&oacute;n de soluciones concretas, sostenibles y, sobre todo, &uacute;tiles para su d&iacute;a a d&iacute;a</div>
 </div>
 <br>
